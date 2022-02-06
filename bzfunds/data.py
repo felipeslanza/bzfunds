0afd6ddb44ce94612f83fcf0917f4877890a0aef
felipeslanza <felipeslanza@gmail.com>
2022-02-06 17:32:31 -0300

    fixed typo in get_monhtly_data and improved tests

--- bzfunds/data.py
+++ bzfunds/data.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from typing import Optional
 
 import pandas as pd
 import requests
@@ -15,19 +16,20 @@ logger = logging.getLogger(__name__)
 
 
 @typechecked
-def get_monthly_data(date: datetime) -> pd.DataFrame:
+def get_monthly_data(date: datetime) -> Optional[pd.DataFrame]:
     url = get_url_from_date(date)
     try:
         res = requests.get(url)
         res.raise_for_status()
     except requests.exceptions.ConnectionError as e:
         logger.error("Connection error")
-    except (requests.exceptions.Timeout, requests.exceptions.HttpError) as e:
+    except (requests.exceptions.Timeout, requests.exceptions.HTTPError) as e:
         logger.error("Service unavailable. Try again later")
     else:
         return parse_data_from_response(res)
 
 
-def get_history(start_dt: datetime, end_dt: datetime) -> pd.DataFrame:
+@typechecked
+def get_history(start_dt: datetime, end_dt: datetime) -> Optional[pd.DataFrame]:
     start_dt = ...
     pass
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import pytest
+from unittest.mock import Mock, patch
+
+import requests
 
 from bzfunds.data import *
 from bzfunds.utils import get_url_from_date
@@ -10,13 +13,23 @@ date_str = "2021-01-01"
 date = pd.to_datetime(date_str)
 
 
-def assert_get_data_is_typed():
-    with pytest.raises(TypeError, match=".*dateitme.*"):
+def test_assert_get_data_is_typed():
+    with pytest.raises(TypeError, match=".*datetime.*"):
         get_monthly_data(date_str)
 
 
-def assert_get_data_only_parses_successul_response():
-    pass
+def test_assert_get_data_only_parses_successul_response():
+    errors = (
+        requests.exceptions.ConnectionError,
+        requests.exceptions.Timeout,
+        requests.exceptions.HTTPError,
+    )
+
+    mocked_get = Mock(side_effect=errors)
+    with patch("bzfunds.data.requests.get", mocked_get):
+        assert get_monthly_data(date) is None
+        assert get_monthly_data(date) is None
+        assert get_monthly_data(date) is None
 
 
 def test_get_history():
