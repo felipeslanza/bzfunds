5c4fa905f17b6a3b4d307d4dae8fbccac45a1cbb
felipeslanza <felipeslanza@gmail.com>
2022-02-11 10:37:19 -0300

    added typeguard to requirements; improved Manager and added initial tests

--- bzfunds/dbm.py
+++ bzfunds/dbm.py
@@ -18,30 +18,44 @@ __all__ = ("Manager",)
 logger = logging.getLogger(__name__)
 
 
+DEFAULT_CLIENT_SETTINGS = {
+    "connectTimeoutMS": 2500,
+    "serverSelectionTimeoutMS": 2500,
+    "retryWrites": True,
+}
+
+
 class Manager:
     def __init__(
         self,
         host: str = "localhost",
         port: int = 27017,
+        *,
         db: str = "bzfundsDB",
         collection: str = "funds",
         username: Optional[str] = None,
         password: Optional[str] = None,
+        **client_settings,
     ):
         self.host = host
         self.port = port
         self.db = db
         self.collection = collection
+        self.client_settings = {**DEFAULT_CLIENT_SETTINGS, **client_settings}
 
+        self.connect()
+
+    def connect(self):
         try:
             self.client = MongoClient(
-                host=host
+                host=host,
                 port=port,
                 username=username,
                 password=password,
+                **self.client_settings,
             )
         except ServerSelectionTimeoutError as e:
             logger.error(f"Failed to connect to database - {e}")
         else:
-            self.db = self.client[db_name]
+            self.db = self.client[db]
             self.collection = self.db[collection]
--- tests/test_dbm.py
+++ tests/test_dbm.py
@@ -1,5 +1,19 @@
+import unittest
+
+import pymongo
+import pytest
+
 from bzfunds.dbm import Manager
 
 
-def test_manager_connection():
-    pass
+class TestDBM(unittest.TestCase):
+    def setUp(self):
+        self.dbm = Manager()
+
+    def tearDown(self):
+        pass
+
+    def test_manager_connection(self):
+        with pytest.raises(pymongo.errors.ServerSelectionTimeoutError):
+            dbm = Manager("invalidhost", serverSelectionTimeoutMS=100)
+            _ = dbm.client.list_databases()
